3b885a64650c8d3966c3435bfe19f66b705aa9d6
Rodion Sarygin <sarygin.rodion7@gmail.com>
2022-11-01 23:12:03 +0700

    added recent files in File menu

--- client/config.py
+++ client/config.py
@@ -6,3 +6,6 @@ SETTINGS_FILE_PATH = os.path.dirname(
 PLUGINS_DIR_PATH = os.path.dirname(
     os.path.realpath(__file__)) + "/plugins/installed"
 DEFAULT_SERVER = "https://5e02-46-181-148-140.eu.ngrok.io"
+RECENT_FILES_DB_PATH = os.path.dirname(
+    os.path.realpath(__file__)) + "/recent_files.sqlite"
+MAX_RECENT_FILES_IN_DB = 10
